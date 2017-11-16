ba5748142bf8292b3ee6a925b8317c818b82ba19
snandlall <snandlall@nyc-mac2-loaner.local>
2017-11-16 12:44:43 -0500

    Project starts succesfully.  Nothing fancy, just a grey background window at this stage

---
+++ ColorBoxSwitches/ColorBoxSwitches/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorBoxSwitches
+//
+//  Created by snandlall on 11/16/17.
+//  Copyright © 2017 snandlall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.lightGray 
+        
+
+    
+    
+    }
+
+
+}
+
