84229beb9dcf7e222f23c7ed0f9606fd449050d6
surnan <forsuresh@yandex.com>
2017-11-18 15:54:53 -0500

    Removed outlet declarations.  Combination Square does not reflect changes in switch value (but print statements verify when they occur).  UI elements (switches and subview aredeclared at top of VC.  Not within a function

--- ColorBoxSwitches/ColorBoxSwitches/ViewController.swift
+++ ColorBoxSwitches/ColorBoxSwitches/ViewController.swift
@@ -10,72 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var redSwitch: UISwitch!
-    var greenSwitch: UISwitch!
-    var blueSwitch: UISwitch!
+    let redSwitch = UISwitch(frame: CGRect(x: 150, y: 50, width: 0, height: 0))
+    let greenSwitch = UISwitch(frame: CGRect(x: 150, y: 100, width: 0, height: 0))
+    let blueSwitch = UISwitch(frame: CGRect(x: 150, y: 150, width: 0, height: 0))
+    let combinationSquare = UIView(frame: CGRect(x: 100, y: 250, width: 200, height: 200))
+
     
-    
-    func initEverything(){
+    func setupUI(){
         view.backgroundColor = UIColor.lightGray
-        redSwitch = UISwitch()
-        greenSwitch = UISwitch()
-        blueSwitch = UISwitch()
-        
-        redSwitch = UISwitch(frame: CGRect(x: 150, y: 50, width: 0, height: 0))
-        greenSwitch = UISwitch(frame: CGRect(x: 150, y: 100, width: 0, height: 0))
-        blueSwitch = UISwitch(frame: CGRect(x: 150, y: 150, width: 0, height: 0))
-        
+       
         redSwitch.tintColor = UIColor.red
         redSwitch.onTintColor = UIColor.red
+        redSwitch.isOn = false
+        self.view.addSubview(redSwitch)
+        redSwitch.addTarget(self, action: #selector(self.switchValueDidChange), for: .valueChanged)
+  
         greenSwitch.tintColor = UIColor.green
         greenSwitch.onTintColor = UIColor.green
+        greenSwitch.isOn = false
+        self.view.addSubview(greenSwitch)
+        greenSwitch.addTarget(self, action: #selector(self.switchValueDidChange), for: .valueChanged)
+
         blueSwitch.tintColor = UIColor.blue
         blueSwitch.onTintColor = UIColor.blue
-        
-        
-        redSwitch.addTarget(self, action: #selector(self.switchValueDidChange), for: .valueChanged)
-        greenSwitch.addTarget(self, action: #selector(self.switchValueDidChange), for: .valueChanged)
+        blueSwitch.isOn = false
+        self.view.addSubview(blueSwitch)
         blueSwitch.addTarget(self, action: #selector(self.switchValueDidChange), for: .valueChanged)
 
-        
-        self.view.addSubview(redSwitch)
-        self.view.addSubview(greenSwitch)
-        self.view.addSubview(blueSwitch)
-
-        
+        combinationSquare.backgroundColor = UIColor(displayP3Red: 0, green: 0, blue: 0, alpha: 1)
+      //  self.combinationSquare = combinationSquare
+        self.view.addSubview(combinationSquare)
+    }
+    
+    func setCombinationSquare(){
+ //       self.redSwitch.isOn = true
+//        combinationSquare.backgroundColor = UIColor(displayP3Red: r, green: g, blue: b, alpha: 1)
+    // willset & didset
         
     }
-  
-    
     
     @objc func switchValueDidChange(to sender: UISwitch)
     {
-        if sender.isOn {
-            print("Switch turned ON")
-        } else {
-            print("OFF")
-        }
+        sender.isOn ? print("Switch turned ON") : print("OFF")
+//        setCombinationSquare() <-- switch initializes with nil.  First value isn't assigned till AFTER we return
     }
-    
-    
-    
-//    @objc func sliderValueDidChange(to value: UISwitch)
-//    {
-//        print("value--")
-//    }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        initEverything()
+        setupUI()
     }
-
     
-    
-    
-    
-    
-    
-
 }
 
