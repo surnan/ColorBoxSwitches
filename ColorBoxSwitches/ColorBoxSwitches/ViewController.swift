aed231661f8363e8ce78aed8813a8b6dc83da573
surnan <forsuresh@yandex.com>
2017-11-18 01:02:12 -0500

    Switches for r,g,b were added and they are each triggering action through selector.  However, i'm unable to determine who is the sender within the obj-c function.  Visually, the switches look good

--- ColorBoxSwitches/ColorBoxSwitches/ViewController.swift
+++ ColorBoxSwitches/ColorBoxSwitches/ViewController.swift
@@ -10,16 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var redSwitch: UISwitch!
+    var greenSwitch: UISwitch!
+    var blueSwitch: UISwitch!
+    
+    
+    func initEverything(){
+        view.backgroundColor = UIColor.lightGray
+        redSwitch = UISwitch()
+        greenSwitch = UISwitch()
+        blueSwitch = UISwitch()
+        
+        redSwitch = UISwitch(frame: CGRect(x: 150, y: 50, width: 0, height: 0))
+        greenSwitch = UISwitch(frame: CGRect(x: 150, y: 100, width: 0, height: 0))
+        blueSwitch = UISwitch(frame: CGRect(x: 150, y: 150, width: 0, height: 0))
+        
+        redSwitch.tintColor = UIColor.red
+        redSwitch.onTintColor = UIColor.red
+        greenSwitch.tintColor = UIColor.green
+        greenSwitch.onTintColor = UIColor.green
+        blueSwitch.tintColor = UIColor.blue
+        blueSwitch.onTintColor = UIColor.blue
+        
+        
+        redSwitch.addTarget(self, action: #selector(self.switchValueDidChange), for: .valueChanged)
+        greenSwitch.addTarget(self, action: #selector(self.switchValueDidChange), for: .valueChanged)
+        blueSwitch.addTarget(self, action: #selector(self.switchValueDidChange), for: .valueChanged)
+
+        
+        self.view.addSubview(redSwitch)
+        self.view.addSubview(greenSwitch)
+        self.view.addSubview(blueSwitch)
+
+        
+        
+    }
+  
+    
+    
+    @objc func switchValueDidChange(to sender: UISwitch)
+    {
+        if sender.isOn {
+            print("Switch turned ON")
+        } else {
+            print("OFF")
+        }
+    }
+    
+    
+    
+//    @objc func sliderValueDidChange(to value: UISwitch)
+//    {
+//        print("value--")
+//    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = UIColor.lightGray 
-        
-
-    
-    
+        initEverything()
     }
 
+    
+    
+    
+    
+    
+    
 
 }
 
